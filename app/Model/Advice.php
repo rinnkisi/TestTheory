f4b058a29d6cea141b6aaf8d1f4388a81367c06c
林貴史 <hayashitakashi@rinnkisi-no-MacBook-Air.local>
2015-12-08 22:37:47 +0900

    過去コードの削除

--- app/Model/Advice.php
+++ app/Model/Advice.php
@@ -24,7 +24,7 @@ class Advice extends AppModel{
         endforeach;
 
         //人数や、平均点、最高点、など
-        $people = $key + 1;
+        $people = count($array_file);
         $average = $this->score_average($score, $people);
         //普遍分散を返す
         $score_dispersion = $this->score_dispersion($score, $average);
@@ -32,10 +32,8 @@ class Advice extends AppModel{
         $low_score = min($score);
         // 問題数をとってくる関数
         $item_sum = $this->item_sum($array_line);
-        debug($item_sum);
         //項目困難度の算出
         $item_difficulty = $this->item_difficulty($item_sum, $people);
-        debug($item_difficulty);
 
         //項目の得点合計や平均点のところ
         //ここは項目の困難度とクロンバックで使う項目分散のところ
@@ -45,300 +43,9 @@ class Advice extends AppModel{
         // 196点となる。また、2人が不正解で不正解者は0点となっている
         // ここでの平均点は合計点の200 / 100は2つまり配点を定める必要がでてくる。1.98点となる
         // ただし、1点を基準としてやる問題であれば 1 で問題はない。
-        $item_sum_dispersion = $this->item_sum_dispersion($people, $item_sum, 1);
-        debug($item_sum_dispersion);
+        $item_dispersion = $this->item_dispersion($people, $item_sum, 1);
+        $cronbach = $this->cronbach(count($item_sum), $item_dispersion, $score_dispersion);
         
-        /*
-        foreach ($array_file as $firstkey => $firstvalue)
-        {
-            $Array[$firstkey] = explode(",", $array_file[$firstkey]);
-            $score[$firstkey] = 0;
-            foreach ($Array[$firstkey] as $key => $value)
-            {
-                if($key == 0)
-                {
-                }
-                else
-                {
-                    $score[$firstkey] += $value;//素点を求める
-                    //値がはいっているかの条件文
-                    if(isset($kourui[$key]))
-                    {
-                        //項目ー平均点
-                        $vtmp = $value - $difficulty[$key];
-                        $kourui[$key] += ($vtmp*$vtmp);
-                    }
-                    else
-                    {
-                        $kourui[$key] = 0;
-                        $vtmp = $value - $difficulty[$key];
-                        $kourui[$key] += ($vtmp*$vtmp);
-                    }
-                }
-            }
-        }
-        foreach($kourui as $koukey => $value1)
-        {
-            $result = $value1/$firstkey;//分散をだしている
-            if(isset($result_sum))
-            {//値がはいっているときの処理
-                $result_sum += $result;//各項目の分散の合計
-            }
-            else
-            {//$result_sumに値がはいっている。
-                $result_sum = 0;//値がはいってないためエラー回避のため
-                $result_sum += $result;//各項目の分散の合計
-            }
-        }
-
-        //各項目の分散と素点の分散をだしている
-        //クロンバックα係数をだしている
-        $cronbach=($koukey/($koukey-1)*(1-($result_sum/$scorebunsan)));
-
-
-        $scoresort=$score;
-        sort($scoresort);//ここでソートをする
-        //debug($scoresort);
-        $basic = round(($firstkey+1)*0.27,0);//少数第１位以下を四捨五入
-                //echo "<BR><BR>zzz".$basic."zz".$firstkey;
-        $basickey =(($firstkey+1)-$basic);//合計の-基準値を計算し後ろのどこまでを分析するか決めている
-        foreach($scoresort as $key =>$value)
-        {
-            if($key+1 <= $basic)
-            {
-                $sumkey= $key+1;
-                $sumvalue=$value;
-            }
-            else if($key+1 >= $basickey)
-            {
-                $totalkey= $key+1;
-                if($basickey == $key)
-                {
-                    $totalvalue=$value;
-                }
-            }
-        }
-        $totalkey = $totalkey-$basic;
-        $savecount=0;
-        $hojicount=0;
-        foreach($score as $key =>$value)
-        {
-            if($value <= $sumvalue)
-            {
-                    $savekey[] = $key;
-                    $savecount+=1;
-            }
-            else if($value >= $totalvalue)
-            {
-                    $hojikey[] = $key;
-                    $hojicount+=1;
-            }
-        }
-        foreach ($array_file as $firstkey => $firstvalue)
-        {
-            $Array[$firstkey] = explode(",", $array_file[$firstkey]);
-            foreach ($Array[$firstkey] as $key => $value)
-            {
-                if($key == 0)
-                {
-                }
-                else
-                {
-                    foreach($savekey as $ley => $x)
-                    {
-                        if($firstkey == $x)
-                        {
-                            if(isset($validity_save[$key]))
-                            {
-                                $validity_save[$key] += $value;
-                            }
-                            else
-                            {
-                                $validity_save[$key] = 0;
-                                $validity_save[$key] += $value;
-                            }
-                        }
-                    }
-                    //上位を求める
-                    foreach ($hojikey as $ley => $y)
-                    {
-                        if($firstkey == $y)
-                        {
-                            if(isset($validity_hoji[$key]))
-                            {
-                                $validity_hoji[$key] += $value;
-                            }
-                            else
-                            {
-                                $validity_hoji[$key] = 0;
-                                $validity_hoji[$key] += $value;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        //項目識別力を求める
-        //上位の成績ひく下位の成績の人の正答率
-        foreach ($validity_hoji as $key => $value)
-        {
-            $result_hoji = $value/$hojicount . "<BR>";//成績上位者の正答率hojicountの値が必ずしもbasicと同じではない
-            $result_save = $validity_save[$key]/$savecount;//成績下位者の正答率
-            $discernment[$key] = $result_hoji - $result_save;
-        }
-        //項目特性図を書くために必要なこと
-        foreach($score as $key =>$value)
-        {
-            $count[$key]=$key;
-        }
-        for($j=0;$j<$firstkey+1;$j++)
-        {
-            $score[$j];
-            for($i=$j;$i<$firstkey+1;$i++)
-            {//ループで最小値を値をさがす
-                if($score[$i] < $score[$j])
-                {
-                    $tmp=$score[$j];
-                    $t=$count[$j];
-                    $score[$j]=$score[$i];
-                    $count[$j]=$count[$i];
-                    $score[$i]=$tmp;
-                    $count[$i] =$t;
-            //keyの値を保持する
-                }
-            }
-        }
-     //5グループに分ける処理
-        $basic_number = ceil($firstkey/5);
-
-        //５群にわけた正答率をだす部分↓
-        foreach($count as $key=>$value)
-        {
-            foreach ($Array[$value] as $secondkey => $secondvalue)
-            {
-                if(0!=$secondkey)
-                {
-                    if($key < $basic_number)
-                    {
-                        if(isset($zu1[$secondkey]))
-                        {
-                            $zu1[$secondkey]+=$secondvalue;
-                        }
-                        else
-                        {
-                            $zu1[$secondkey] = 0;
-                            $zu1[$secondkey]+=$secondvalue;
-                        }
-                    }elseif($key < $basic_number*2)
-                    {
-                        if(isset($zu2[$secondkey]))
-                        {
-                            $zu2[$secondkey]+=$secondvalue;
-                        }
-                        else
-                        {
-                            $zu2[$secondkey] = 0;
-                            $zu2[$secondkey]+=$secondvalue;
-                        }
-                    }elseif($key < $basic_number*3)
-                    {
-                        if(isset($zu3[$secondkey]))
-                        {
-                            $zu3[$secondkey]+=$secondvalue;
-                        }
-                        else{
-                            $zu3[$secondkey] = 0;
-                            $zu3[$secondkey]+=$secondvalue;
-                        }
-                    }
-                    elseif($key < $basic_number*4)
-                    {
-                        if(isset($zu4[$secondkey]))
-                        {
-                            $zu4[$secondkey]+=$secondvalue;
-                        }
-                        else
-                        {
-                            $zu4[$secondkey] = 0;
-                            $zu4[$secondkey]+=$secondvalue;
-                        }
-                    }
-                    else
-                    {
-                        if(isset($zu5[$secondkey]))
-                        {
-                            $zu5[$secondkey]+=$secondvalue;
-                        }
-                        else
-                        {
-                            $zu5[$secondkey] = 0;
-                            $zu5[$secondkey]+=$secondvalue;
-                        }
-                    }
-                }
-            }
-        }
-        for($j=1;$j < $secondkey+1;$j++)
-        {
-            if(isset($hozon1[$j]))
-            {
-                $hozon1[$j] = $zu1[$j]/$basic_number;
-            }
-            else
-            {
-                $hozon1[$j] = 0;
-                $hozon1[$j] = $zu1[$j]/$basic_number;
-            }
-        }
-        for($j=1;$j < $secondkey+1;$j++)aaaaaaaaaaa
-        {
-            if(isset($hozon2[$j]))
-            {
-                $hozon2[$j] = $zu2[$j]/$basic_number;
-            }
-            else
-            {
-                $hozon2[$j] = 0;
-                $hozon2[$j] = $zu2[$j]/$basic_number;
-            }
-        }
-        for($j=1;$j < $secondkey+1;$j++)
-        {
-            if(isset($hozon3[$j]))
-            {
-                $hozon3[$j] = $zu3[$j]/$basic_number;
-            }
-            else
-            {
-                $hozon3[$j] = 0;
-                $hozon3[$j] = $zu3[$j]/$basic_number;
-            }
-        }
-        for($j=1;$j < $secondkey+1;$j++)
-        {
-            if(isset($hozon4[$j]))
-            {
-                $hozon4[$j] = $zu4[$j]/$basic_number;
-            }
-            else
-            {
-                $hozon4[$j] = 0;
-                $hozon4[$j] = $zu4[$j]/$basic_number;
-            }
-        }
-        for($j=1;$j < $secondkey+1;$j++)
-        {
-            if(isset($hozon5[$j]))
-            {
-                $hozon5[$j] = $zu5[$j]/$basic_number;
-            }
-            else
-            {
-                $hozon5[$j] = 0;
-                $hozon5[$j] = $zu5[$j]/$basic_number;
-            }
-        }
-*/
         $file->close();
         //return array($discernment, $difficulty, $cronbach);
     }
@@ -368,7 +75,7 @@ class Advice extends AppModel{
         }
         return $sum / $basic;
     }
-    // 不偏分散を返す(分散の場合はkeyをプラスしない)
+    // 分散を返す(分散の場合はkeyをプラスしない)
     public function score_dispersion($score = array(), $average = null)
     {
         $sum = 0;
@@ -377,8 +84,7 @@ class Advice extends AppModel{
             $tmp[$key] = (($value - $average) * ($value - $average));
             $sum += $tmp[$key];
         }
-        echo $key;
-        return $sum / ($key + 1);
+        return $sum / count($score);
     }
     public function item_difficulty($item = array(), $basic = null)
     {
@@ -405,14 +111,20 @@ class Advice extends AppModel{
         return $item_sum;
     }
     // 項目毎の分散値を求めてその合計値を返す関数 項目合計分散値
-    public function item_sum_dispersion($people = null, $right_user = array(), $basic_point)
+    public function item_dispersion($people = null, $right_user = array(), $basic_number)
     {
         foreach($right_user as $key => $right_value):
-            $right_average = pow(($basic_point - ($right_value / $people)), 2) * $right_value;
+            $right_average = pow(($basic_number - ($right_value / $people)), 2) * $right_value;
             $incorrect_average = pow(- ($right_value / $people), 2) * ($people - $right_value);
             $item_dispersion[$key] = ($right_average + $incorrect_average) / $people;
         endforeach;
         return array_sum($item_dispersion);
+    }
+    // クロンバックのα係数を求める式がα＝項目数 /（項目数-1）×（1-(各項目の分散の合計/合計点の分散）
+    public function cronbach($item = null, $item_dispersion, $score_dispersion)
+    {
+        $cronbach = ($item / ($item - 1)) * (1 - ($item_dispersion / $score_dispersion));
+        return $cronbach;
     }
     // CSVファイルの場合は0を返す
     public function file_check($file_name = null)
